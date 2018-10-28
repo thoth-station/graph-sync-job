f5e1a09339f0fae1ec182484e1593222a90e3b0e
Christoph Görn <goern@redhat.com>
2018-10-28 12:02:58 +0100

    some cleanup, and using THOTH_S3_ENDPOINT_URL as default for solver_results_store_host and analysis_results_store_host
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- app.py
+++ app.py
@@ -86,13 +86,15 @@ def _print_version(ctx, _, value):
               show_default=True, metavar=GraphDatabase.ENVVAR_HOST_NAME,
               envvar=GraphDatabase.ENVVAR_HOST_NAME, multiple=True,
               help="Hostname to the graph instance to perform sync to.")
-@click.option('--graph-port', type=int, default=GraphDatabase.DEFAULT_PORT, show_default=True, metavar='HOST',
+@click.option('--graph-port', type=int, default=GraphDatabase.DEFAULT_PORT, show_default=True, metavar=GraphDatabase.ENVVAR_HOST_PORT,
               envvar=GraphDatabase.ENVVAR_HOST_PORT,
               help="Port number to the graph instance to perform sync to.")
-@click.option('--solver-results-store-host', type=str, show_default=True, metavar='HOST', default=None,
-              help="Hostname to solver results store from which the sync should be performed.")
-@click.option('--analysis-results-store-host', type=str, show_default=True, metavar='HOST', default=None,
-              help="Hostname to analysis results store from which the sync should be performed.")
+@click.option('--solver-results-store-host', type=str, show_default=True, envvar='THOTH_S3_ENDPOINT_URL',
+              metavar='URL', default=None,
+              help="URL of solver results store from which the sync should be performed.")
+@click.option('--analysis-results-store-host', type=str, show_default=True, envvar='THOTH_S3_ENDPOINT_URL',
+              metavar='URL', default=None,
+              help="URL of analysis results store from which the sync should be performed.")
 @click.option('-v', '--verbose', is_flag=True, envvar='THOTH_GRAPH_SYNC_DEBUG',
               help="Be more verbose about what's going on.")
 @click.option('--force-solver-results-sync', is_flag=True, envvar='THOTH_GRAPH_SYNC_FORCE_SOLVER_RESULTS_SYNC',
@@ -100,7 +102,7 @@ def _print_version(ctx, _, value):
 @click.option('--force-analysis-results-sync', is_flag=True, envvar='THOTH_GRAPH_SYNC_FORCE_ANALYSIS_RESULTS_SYNC',
               help="Force sync of solver results regardless if they exist (duplicate entries will not be created).")
 @click.option('--metrics-pushgateway-url', type=str, default='pushgateway:80', show_default=True,
-              envvar='THOTH_METRICS_PUSHGATEWAY_URL',
+              envvar='THOTH_METRICS_PUSHGATEWAY_URL', metavar='URL',
               help="Send job metrics to a Prometheus pushgateway URL.")
 def cli(verbose, solver_results_store_host, analysis_results_store_host, graph_hosts, graph_port,
         force_solver_results_sync, force_analysis_results_sync, metrics_pushgateway_url):
@@ -140,7 +142,7 @@ def cli(verbose, solver_results_store_host, analysis_results_store_host, graph_h
 
         analysis_store = AnalysisResultsStore(host=analysis_results_store_host)
         analysis_store.connect()
-        _LOGGER.info(f"Syncing image analysis results to {graph_hosts}")
+        _LOGGER.info(f"Syncing image analysis results from {analysis_results_store_host} to {graph_hosts}")
         for document_id, document in analysis_store.iterate_results():
             _METRIC_ANALYSIS_RESULTS_PROCESSED.inc()
 
